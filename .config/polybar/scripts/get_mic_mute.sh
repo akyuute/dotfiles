a9cfd3a87fcccbc517e7fbf21a678968edf809d0
Sam <trackmenot10@gmail.com>
2024-02-29 23:16:08 -0800

    Reimplement pulseaudio; Add script options

--- .config/polybar/scripts/get_mic_mute.sh
+++ .config/polybar/scripts/get_mic_mute.sh
@@ -1,7 +1,59 @@
 #!/bin/bash
 
-mixer='Capture'
+# Use pactl to get the current microphone state.
+# Echo a muted or unmuted icon, which can be set manually.
+# The name of a source device is required as a positional argument.
+
+# Syntax:
+# -h (Print a help message)
+# -m MUTED_ICON
+# -u UNMUTED_ICON
+
+Help()
+{
+    echo "Syntax: $0 SOURCE -[h|m|u] [...]"
+    echo "        SOURCE - Audio source device name"
+    echo "        (Usually something crazy like 'alsa_input.pci-0000_00_1f.3- ...')"
+    echo ""
+    echo "Options:"
+    echo "-h  (Print this help message and exit)"
+    echo "-m  MUTED_ICON"
+    echo "-u  UNMUTED_ICON"
+}
+
+if [ $# == 0 ];
+then
+    echo "Error: Invalid syntax" 2>&1
+    Help
+    exit 1
+fi
+
+src=$1
 icon_muted=''
 icon_unmuted=''
-state=`amixer get $mixer | sed -En 's/.*\[(.*)\]/\1/p'`
-[[ "$state" =~ "on" ]] && echo $icon_unmuted || echo $icon_muted
+
+
+while getopts "h:m:u:" opt;
+do
+    case $opt in
+        h)
+            Help
+            exit 0
+            ;;
+
+        m)
+            icon_muted=$OPTARG
+            ;;
+        u)
+            icon_unmuted=$OPTARG
+            ;;
+        \?)
+            echo "Invalid syntax"
+            Help
+            exit 1 ;;
+
+    esac
+done
+
+muted=`pactl get-source-mute "$src" | sed -n 's/Mute: //p'`
+[[ "$muted" =~ "yes" ]] && echo $icon_muted || echo $icon_unmuted
