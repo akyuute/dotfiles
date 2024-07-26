c5188d3636d58960643ab67eebde09517aea39db
Samantha <trackmenot10@gmail.com>
2024-07-26 02:02:12 -0700

    Implement multi-monitor bar, ws pinning; Dolphin FM; blank screen vs. i3lock

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,5 +1,32 @@
-# polybar-msg cmd quit
-killall -q polybar
+#!/bin/bash
 
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar kagami 2>&1 | tee -a /tmp/polybar1.log & disown
+# Credit: @wouterh-dev
+
+(
+  flock 200
+
+  killall -q polybar
+
+  while pgrep -u $UID -x polybar > /dev/null; do sleep 0.5; done
+
+  outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
+  tray_output=eDP1
+
+  for m in $outputs; do
+    if [[ $m == "HDMI1" ]]; then
+        tray_output=$m
+    fi
+  done
+
+  for m in $outputs; do
+    export MONITOR=$m
+    export TRAY_POSITION=none
+    if [[ $m == $tray_output ]]; then
+      TRAY_POSITION=right
+    fi
+
+    polybar --reload kagami </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
+    disown
+  done
+) 200>/var/tmp/polybar-launch.lock
+
