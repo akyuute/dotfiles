85d143ea971c46fdc51835d46392f04797ffeea8
Samantha <trackmenot10@gmail.com>
2024-10-20 04:36:37 -0700

    Add Neovim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,50 @@
+require("core.options") -- Load general options
+--require 'core.keymaps' -- Load general keymaps
+require("core.snippets") -- Custom code snippets
+
+-- Set up the Lazy plugin manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Set up plugins
+require("lazy").setup({
+	require("plugins.mine"),
+	-- require 'plugins.neotree',
+	-- require 'plugins.colortheme',
+	-- require 'plugins.bufferline',
+	-- require 'plugins.lualine',
+	require("plugins.treesitter"),
+	require("plugins.telescope"),
+	require("plugins.lsp"),
+	require("plugins.autocompletion"),
+	-- require("plugins.none-ls"),
+	require("plugins.gitsigns"),
+	require("plugins.alpha"),
+	-- require 'plugins.indent-blankline',
+	-- require 'plugins.misc',
+	-- require 'plugins.comment',
+	{
+		"numToStr/Comment.nvim",
+	},
+})
+
+-- Bind ^/ to toggle comment
+local api = require("Comment.api")
+vim.keymap.set("n", "<C-_>", api.toggle.linewise.current)
+-- Disable autocomment
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "*",
+	callback = function()
+		vim.opt_local.formatoptions:remove({ "r", "o" })
+	end,
+})
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
